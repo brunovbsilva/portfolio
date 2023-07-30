ffdf7de6cfc8a38b52ee9ee2dd2e34d59acffd3d
Bruno Silva <brunovinicius0704@gmail.com>
2023-07-30 15:31:26 -0300

    removendo alterações

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   { 
-    path: 'portifolio',
+    path: '',
     component: LayoutComponent,
     children: [
       { path: '', component: MainPageComponent },
