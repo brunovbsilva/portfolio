3d3ed4ee60dae0e07e0ce6b702d7ee5445cdaccd
Bruno Silva <brunovinicius0704@gmail.com>
2023-09-24 14:07:07 -0300

    add personal project

--- src/app/consts/projects.const.ts
+++ src/app/consts/projects.const.ts
@@ -4,6 +4,15 @@ export const projectsEN : ProjectsModel = {
     header: 'Projects',
     projects: [
       {
+        title: "AirFinder",
+        description: "Personal project that aims to facilitate the search for Air-Soft games. It was developed in C# and Angular, it is not released yet and it is not hosted anywhere.",
+        imagePath: 'assets/images/air-finder.png',
+        buttons: [
+          { type: 'code', link: 'https://gitfront.io/r/brunovbsilva/h58r3DCZ9UpB/air-finder/', tooltip: 'View back-end code' },
+          { type: 'code', link: 'https://gitfront.io/r/brunovbsilva/kDFY9r3UuLhW/angular-air-finder/', tooltip: 'View front-end code' },
+        ]
+      },
+      {
         title: "Portfolio",
         description: "This project is my personal portfolio. It was developed using Angular and Material Design. It is a SPA (Single Page Application) and is hosted on Github Pages.",
         imagePath: 'assets/images/portfolio.png',
@@ -12,17 +21,21 @@ export const projectsEN : ProjectsModel = {
           { type: 'open_in_new', link: 'https://brunovbsilva.github.io/portfolio/', tooltip: 'Open project' },
         ]
       },
-      { 
-        title: 'Under construction...',
-        description: "Under construction. There will be more features soon...",
-        loading: true
-      },
     ]
 };
 
 export const projectsPT : ProjectsModel = {
     header: 'Projetos',
     projects: [
+      {
+        title: "AirFinder",
+        description: "Projeto pessoal que visa facilitar a procura por jogos de Air-Soft. Foi desenvolvido em C# e Angular, ainda não foi lançado e não está hospedado em nenhum lugar.",
+        imagePath: 'assets/images/air-finder.png',
+        buttons: [
+          { type: 'code', link: 'https://gitfront.io/r/brunovbsilva/h58r3DCZ9UpB/air-finder/', tooltip: 'Ver código back-end' },
+          { type: 'code', link: 'https://gitfront.io/r/brunovbsilva/kDFY9r3UuLhW/angular-air-finder/', tooltip: 'Ver código front-end' },
+        ]
+      },
       {
         title: "Portfólio",
         description: "Este projeto é o meu portfólio pessoal. Foi desenvolvido utilizando Angular e Material Design. É uma SPA (Single Page Application) e está hospedado no Github Pages.",
@@ -32,10 +45,5 @@ export const projectsPT : ProjectsModel = {
           { type: 'open_in_new', link: 'https://brunovbsilva.github.io/portfolio/', tooltip: 'Abrir projeto' },
         ]
       },
-      { 
-        title: "Em construção...",
-        description: "Em construção. Em breve terá mais funcionalidades...",
-        loading: true 
-      }
     ]
 };
